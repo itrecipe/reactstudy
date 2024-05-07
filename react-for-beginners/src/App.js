abb883031fc9945156d843a4d1ecc5dc30bf538b
ITRecipe <itrecipe95@gmail.com>
2024-05-07 20:50:54 +0900

    react_study
    24.05.07 학습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,25 +1,36 @@
-//import logo from './logo.svg';
-//import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  //return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  //);
+  //[loading(data), setLoading(수정할 수 있는 함수)]
+  //useEffect 나의 component가 시작할 때만 어떤 코드를 딱 한번만 실행시키고 싶을때 사용
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    
+    //then 방식보다 요즘은 이 방식을 더 많이 쓴다고 한다.
+    const json = await (
+       await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year`
+    )
+  ).json();
+    
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies()
+  }, []);
+
+  console.log(movies);
+
+  return <div>{loading ? <h1>Loading...</h1> : <div>
+    {movies.map(movie =>
+    
+    )}
+    </div>
+    }
+    </div>;
 }
 
 export default App;
---
+++ react-for-beginners/src/Movie.js
@@ -0,0 +1,15 @@
+//Movie 컴포넌트 생성
+function Movie({medium_cover_image, title, summary, genres}) {
+    return 
+    <div>
+        <img src={medium_cover_image} />
+        <h2>{title}</h2>
+        <p>{summary}</p>
+        <ul>
+        {genres.map((g) => 
+        <li key={g}>{g}</li>
+        )}
+        </ul>
+  </div>;
+}
+export default Movie;
