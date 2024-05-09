a409af9baf8dddcd6ef47c234e35b27567716ba2
ITRecipe <itrecipe95@gmail.com>
2024-05-09 22:58:56 +0900

    react_study
    24.05.09

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,7 +2,46 @@ import { useEffect, useState } from "react";
 import Movie from "./components/Movie"
 
 function App() {
-  return null;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    
+    //then 방식보다 요즘은 이 방식을 더 많이 쓴다고 한다.
+    const json = await (
+       await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year`
+    )
+  ).json();
+    
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies()
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <div>
+    {loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div>
+        {movies.map((movie) => (
+          <Movie
+            key={movie.id} //키값을 주는것은 매우중요함
+            coverImg={movie.medium_cover_image} 
+            title={movie.title} 
+            summary={movie.summary} 
+            genres={movie.genres}
+          />
+        ))}
+      </div>
+    )}
+  </div>
+  );
 }
 
 export default App;
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -3,44 +3,44 @@ import Movie from "../components/Movie";
 function Home() {
 //[loading(data), setLoading(수정할 수 있는 함수)]
   //useEffect 나의 component가 시작할 때만 어떤 코드를 딱 한번만 실행시키고 싶을때 사용
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
+  // const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState([]);
+  // const getMovies = async() => {
     
-    //then 방식보다 요즘은 이 방식을 더 많이 쓴다고 한다.
-    const json = await (
-       await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year`
-    )
-  ).json();
+  //   //then 방식보다 요즘은 이 방식을 더 많이 쓴다고 한다.
+  //   const json = await (
+  //      await fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year`
+  //   )
+  // ).json();
     
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
+  //   setMovies(json.data.movies);
+  //   setLoading(false);
+  // };
 
-  useEffect(() => {
-    getMovies()
-  }, []);
+  // useEffect(() => {
+  //   getMovies()
+  // }, []);
 
-  console.log(movies);
+  // console.log(movies);
 
-  return (
-    <div>
-    {loading ? (
-      <h1>Loading...</h1>
-    ) : (
-      <div>
-        {movies.map((movie) => (
-          <Movie
-            key={movie.id} //키값을 주는것은 매우중요함
-            coverImg={movie.medium_cover_image} 
-            title={movie.title} 
-            summary={movie.summary} 
-            genres={movie.genres}
-          />
-        ))}
-      </div>
-    )}
-  </div>
-  );
+  // return (
+  //   <div>
+  //   {loading ? (
+  //     <h1>Loading...</h1>
+  //   ) : (
+  //     <div>
+  //       {movies.map((movie) => (
+  //         <Movie
+  //           key={movie.id} //키값을 주는것은 매우중요함
+  //           coverImg={movie.medium_cover_image} 
+  //           title={movie.title} 
+  //           summary={movie.summary} 
+  //           genres={movie.genres}
+  //         />
+  //       ))}
+  //     </div>
+  //   )}
+  // </div>
+  // );
 }
